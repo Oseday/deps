8383dd602500a8dcab345c2069b35f50ba88d6b9
Oseday <cancakir99@hotmail.com>
2020-01-12 18:30:08 +0300

    Update authent.lua

--- authent.lua
+++ authent.lua
@@ -1,8 +1,11 @@
---local http = require"coro-http"
---local json = require"json"
+local http = require"coro-http"
+local json = require"json"
 local OSS = jit.os=="Windows" and "\\" or "/"
 
-local audCheck = {["680656297638-r9vbgsbimrqueto0r5e9v5ulvmjo0fg1.apps.googleusercontent.com"]=true,}
+local audCheck = {
+	["680656297638-r9vbgsbimrqueto0r5e9v5ulvmjo0fg1.apps.googleusercontent.com"]=true,
+	["680656297638-r9vbgsbimrqueto0r5e9v5ulvmjo0fg1.apps.googleusercontent.com"]=true,
+}
 
 function TableToString(t)
 	local s = "{"
@@ -34,11 +37,12 @@ local UsersFileAppend = io.open("."..OSS.."users"..OSS.."users", "a")
 
 do --get latest users at start
 	local UsersFileRead = io.open("."..OSS.."users"..OSS.."users", "r")
-	local pattern = "sub=[%s-];table=%s-\n"
+	local pattern = "sub=%[(.-)%];table=(.-);"
 	for line in UsersFileRead:lines() do
-		local sub,tab = string.gfind(line, pattern)
+		local _,_,sub,tab = string.find(line, pattern)
+		p(sub,tab)
 		if sub and tab then
-			Users[sub] = loadstring(TableToLoadstringFormat(tab))()
+			Users[sub] = loadstring("return "..tab)()
 		end
 	end
 	UsersFileRead:close()
@@ -48,19 +52,28 @@ end
 function createUser(user)
 	local tab = {}
 	Users[user.sub] = tab
-	local str = ("sub=[%s];table=%s\n"):format(user.sub, TableToString(tab))
+	local str = ("sub=[%s];table=%s;\n"):format(user.sub, TableToString(tab))
 	UsersFileAppend:write(str)
 	UsersFileAppend:flush()
 end
 
 function validateToken(token) --https://oauth2.googleapis.com/tokeninfo?id_token=XYZ123
-		local uri = "https://oauth2.googleapis.com/tokeninfo?id_token="..token
-		local res, body = pcall(http.request,"GET",uri)
-		if res then
-			if not audCheck[body.aud] then return false,"Invalid aud",400 end
-			return true,body,200
-		else print("ERROR: ",body) return false,body,500
-		end
+	local uri = "https://oauth2.googleapis.com/tokeninfo?id_token="..token
+	local suc, res, body = pcall(http.request,"GET",uri)
+	if not suc or not res then return false,body or res,500 end
+
+	print("TOKEN:")
+	print(body)
+
+	body = json.decode(body)
+
+
+	if body.error and body.error_description then return false,body.error_description,res.code or 500 end
+
+	if res.code and res.code>=400 then return false,"Check error code",res.code end
+
+	if not audCheck[body.aud] then return false,"Invalid aud",400 end
+	return true,body,200
 end
 
 function module.login(token)
@@ -80,11 +93,11 @@ function module.login(token)
 	return true,"Success",200
 end
 
-function module.userAccess(token)
+function module.tokenLoggedin(token)
 	local sub = Tokens[token]
 	if sub then
 		if Users[sub].token == token then
-			return true,"Valid",200
+			return sub,"Valid",200
 		else
 			local user do
 				local s,n,e = validateToken(token)
@@ -94,7 +107,7 @@ function module.userAccess(token)
 			if sub ~= user.sub then return false,"Token sub mismatch",500 end
 			Tokens[Users[sub].token] = nil
 			Users[sub].token = token
-			return true,"Valid new token",200
+			return sub,"Valid new token",200
 		end
 	else
 		return false,"Invalid token",401
@@ -105,9 +118,9 @@ end
 function module.setupServer(server)
 	if server then--for debugging
 
-		server:post("/analytics/auth/login", function(req, res)--Website home page
+		server:post("/analytics/auth/login", function(req, res)
 			coroutine.wrap(function()
-				local token = req.pargams.token
+				local token = req.body.token
 				if token then
 					local s,n,e = module.login(token)
 					res:send(n,e)
@@ -116,14 +129,8 @@ function module.setupServer(server)
 				end
 			end)()
 		end)
-
-		server:post("/analytics/auth/login", function(req, res)--Website home page
-			coroutine.wrap(function()
-
-			end)()
-		end)
-
-
+		
+		
 	end
 end
 
