dc9a15954818c555655f0d29655b0324c04ca4d3
Oseday <cancakir99@hotmail.com>
2020-01-16 22:31:05 +0300

    Update quickio.lua

--- quickio.lua
+++ quickio.lua
@@ -19,4 +19,31 @@ local function write(f,...)
 	return f,w
 end
 
-return {read=read,write=write}
+local function removeLine(filename, starting_line, num_lines)
+    local fp = io.open(filename, "r")
+    if fp == nil then return nil end
+ 
+    content = {}
+    i = 1;
+    for line in fp:lines() do
+        if i < starting_line or i >= starting_line + num_lines then
+	    content[#content+1] = line
+	end
+	i = i + 1
+    end
+ 
+    if i > starting_line and i < starting_line + num_lines then
+	print("Warning: Tried to remove lines after EOF.")
+    end
+ 
+    fp:close()
+    fp = io.open(filename, "w+")
+ 
+    for i = 1, #content do
+	fp:write(string.format("%s\n", content[i]))
+    end
+ 
+    fp:close()
+end
+
+return {read=read,write=write,removeLine=removeLine}
