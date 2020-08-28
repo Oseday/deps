f4d211b0673a0c64cf7fb5b04017328dded49f0b
Oseday <cancakir99@hotmail.com>
2020-08-28 04:37:22 +0300

    photos

---
+++ itu/photos.lua
@@ -0,0 +1,66 @@
+local mc
+if jit.os=="Windows"then
+	mc = "mooncake"
+else
+	mc = "depsMoonCake/mooncake"
+end
+
+local fs = require"fs"
+local json = require"json"
+local timer = require"timer"
+local helpers = require(mc.."/libs/helpers")
+local tick = function() return helpers.getTime()/1000 end
+
+local quickio = require"ose/quickio"
+local pafix = require"ose/pafix"
+local rndid = require"ose/rndid"
+
+local OSS = jit.os=="Windows" and "\\" or "/"
+
+function direxists(dir) 
+	return os.execute("[ -d itu"..OSS..dir.." ]")==true
+end
+
+function makedir(dir)
+	return os.execute("mkdir itu"..OSS..dir)
+end
+
+function removedir(dir)
+	return os.execute("rm -rf itu"..OSS..dir)
+end
+
+local function TableToString(t)
+	local s = "{"
+	for k,v in pairs(t) do
+		if type(v)=="string" then v = "[==["..v.."]==]" end
+		local ts = type(v)=="table" and TableToString(v) or v
+		if ts==true then ts="true" elseif ts==false then ts="false" end
+		if type(k)=="number" then
+			s=s.."["..k.."]="..ts..","
+		else
+			s=s.."['"..k.."']="..ts..","
+		end
+	end
+	return s.."}"
+end
+
+function TableToLoadstringFormat(t)
+	return "return "..TableToString(t)
+end
+
+local Photos = {}
+
+function addphoto(locname,animalname,dir)
+	
+end
+
+
+function module.setupServer(server)
+	server:post("/photos/:locname", function(req, res)
+		local username = req.params.locname
+		
+		p(req.body)
+	end)
+end
+
+return module
