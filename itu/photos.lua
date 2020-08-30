fac49a9f93050f9bf70e00b6832d59fbe2952569
Oseday <cancakir99@hotmail.com>
2020-08-30 13:24:03 +0300

    pht

--- itu/photos.lua
+++ itu/photos.lua
@@ -56,6 +56,27 @@ function TableToLoadstringFormat(t)
 	return "return "..TableToString(t)
 end
 
+function SaveTable(tab,path)
+	local f,w = quickio.write(pafix("itu/"..path), TableToLoadstringFormat(tab))
+	if not f then print("ERROR: Couldn't save "..path..":", w) end
+end
+
+function LoadTable(tab,path)
+	local f,w = quickio.read(pafix("itu/"..path))
+	if f then
+		for k in pairs(tab) do
+			tab[k]=nil
+		end
+		local temp = loadstring(f)()
+		for k,v in pairs(temp) do
+			tab[k]=v
+		end
+	else
+		print("Error opening itu/"..path..":", w)
+		SaveTable(tab,path)
+	end
+end
+
 local ITUDir = _G.EXECPATH .. OSS.."deps/ose/itu/websites"
 local PhotoDir = _G.EXECPATH ..OSS.. "itu" ..OSS.. "photos" ..OSS
 
@@ -65,20 +86,38 @@ local Photos = {
 	}
 }
 
+LoadTable(Photos,"photosmeta")
+
 function addphoto(locname,animalname,photoname,tempdir)
 	local loc = PhotoDir..locname
-	p(fs.mkdir(loc))
+	fs.mkdir(loc)
 	local aniloc = loc..OSS..animalname
-	p(fs.mkdir(aniloc))
+	fs.mkdir(aniloc)
 
 	if not Photos[locname] then Photos[locname]={} end
 	if not Photos[locname][animalname] then Photos[locname][animalname]={} end
 	
-	table.insert(Photos[locname][animalname],photoname)
+	table.insert(Photos[locname][animalname], photoname)
+
+	SaveTable(Photos,"photosmeta")
 
 	local err,notf = fs.renameSync(tempdir, aniloc..OSS..photoname)
 	if err == nil then return false,notf,500 end
 	return true,"success",200
+end
+
+function deletephoto(locname,animalname,photoname)
+	if not Photos[locname] or not Photos[locname][animalname] then return false,"no such loc-animal exists",403 end
+
+	local t,b = Photos[locname][animalname]
+	for i,v in pairs(t) do
+		if v==photoname then Photos[locname][animalname][i]=nil b=v break end
+	end
+	if not b then return false,"no such photo exists",403 end
+
+	fs.unlink(PhotoDir..locname..OSS.animalname..OSS..photoname,function()end)
+
+	SaveTable(Photos,"photosmeta")
 end
 
 
@@ -107,7 +146,13 @@ function module.setupServer(server)
 		local locname = req.params.locname
 		local animalname = req.params.animalname
 		local photoname = req.params.photoname
-		fs.unlink(PhotoDir..locname..OSS.animalname..OSS..photoname,function()end)
+
+		local succ,notf,code = deletephoto(locname,animalname,photoname)
+		if not succ then
+			p("ERROR:",notf)
+			return res:send(notf,code)
+		end
+		
 		res:send("",200)
 	end)
 
