74e7f97a45a763e3fe3fef49767503c6b6ea8170
Oseday <cancakir99@hotmail.com>
2020-09-01 19:25:34 +0300

    Create tests.lua

---
+++ itu/tests.lua
@@ -0,0 +1,44 @@
+local vips = require "vips"
+
+-- fast thumbnail generator
+local image = vips.Image.thumbnail("somefile.jpg", 128)
+image:write_to_file("tiny.jpg")
+
+-- make a new image with some text rendered on it
+image = vips.Image.text("Hello <i>World!</i>", {dpi = 300})
+
+-- call a method
+image = image:invert()
+
+-- use the `..` operator to join images bandwise
+image = image .. image .. image
+
+-- add a constant
+image = image + 12
+-- add a different value to each band
+image = image + { 1, 2, 3 }
+-- add two images
+image = image + image
+
+-- split bands up again
+b1, b2, b3 = image:bandsplit()
+
+-- read a pixel from coordinate (10, 20)
+r, g, b = image(10, 20)
+
+-- make all pixels less than 128 bright blue
+--    :less(128) makes an 8-bit image where each band is 255 (true) if that 
+--        value is less than 128, and 0 (false) if it's >= 128 ... you can use
+---       images or {1,2,3} constants as well as simple values
+--    :bandand() joins all image bands together with bitwise AND, so you get a
+--        one-band image which is true where all bands are true
+--    condition:ifthenelse(then, else) takes a condition image and uses true or
+--        false values to pick pixels from the then or else images ... then and
+--        else can be constants or images
+image = image:less(128):bandand():ifthenelse({ 0, 0, 255 }, image)
+
+-- go to Yxy colourspace
+image = image:colourspace("yxy")
+
+-- pass options to a save operation
+image:write_to_file("x.png", { compression = 9 })
