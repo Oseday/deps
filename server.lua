d474a4c49bed1958e026120ee01a114c0176d463
Oseday <cancakir99@hotmail.com>
2021-03-25 02:17:44 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -78,7 +78,6 @@ do--Info from scraper
 		if client_name ~= PRIVATE_IP then return end
 
 		local url = next(req.body)
-		p(url)
 
 		for name, tab in pairs(returniptable) do
 			table.insert(tab.data, url)
@@ -95,68 +94,6 @@ end
 --  curl --data "URL_GOES_HERE" localhost:351
 --  curl -d "TEST" -X POST localhost:351
 
---[[
-
-local CORS = require"ose/CORS"
-local Authentication = require"ose/authent"
-local Analytics = require"ose/analytics"
-local WebsiteHandle = require"ose/websitehandle"
-
-local isHttps = true
-
-local mc,ip,port,porthhtps
-if jit.os=="Windows" then
-	mc = "mooncake"--"./ndeps/deps/depsMoonCake/mooncake"
-	ip = "127.0.0.1"
-	port = 80
-	isHttps = false
-else
-	mc = "depsMoonCake/mooncake"
-	ip = "172.26.10.180"
-	porthhtps = 443
-	port = 80
-	_G.EXECPATH = _G.EXECPATH .."/"
-end
-
-local MoonCake = require(mc)
-
-
-if isHttps then
-	local ServerHttps = MoonCake:new{
-		isHttps = true, keyPath = _G.EXECPATH.."zerossl-local/keys"
-	}
-	
-	CORS.setupServer(ServerHttps)
-	Analytics.setupServer(ServerHttps)
-	Authentication.setupServer(ServerHttps)
-	WebsiteHandle.setupServer(ServerHttps,MoonCake)
-	
-	ServerHttps:start(porthhtps,ip)
-end
-
-local Server = MoonCake:new()
-
-CORS.setupServer(Server)
-Analytics.setupServer(Server)
-Authentication.setupServer(Server)
-WebsiteHandle.setupServer(Server,MoonCake)
-
-Server:start(port,ip)
-
-Analytics.setupTimer()
-
-
-]]
-
--- git clone https://Oseday:3MicikP7fTvbkDS@github.com/Oseday/Analytics
--- C:\Users\canca\Desktop\Roblox\Vurse\combined-server\Websites\Analytics
--- rm -rf Analytics && git clone https://Oseday:3MicikP7fTvbkDS@github.com/Oseday/Analytics
-
--- cd /home/ec2-user/Websites/Analytics && git fetch origin && git reset --hard origin/master
-
--- git clone https://Oseday:3MicikP7fTvbkDS@github.com/Oseday/ose
-
--- git clone https://Oseday:3MicikP7fTvbkDS@github.com/Oseday/C-builds
-
 -- cd deps/ose && git pull origin master && cd ../.. && sudo ./luvit server.lua
 
+return true
